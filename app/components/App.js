5a6df5aadb6f28d9a10883eabf760f3f9b6f8998
Daniel Brady <daniel.brady@tapjoy.com>
2017-09-12 10:28:27 -0400

    Add initial page structure

--- app/components/App.js
+++ app/components/App.js
@@ -1,7 +1,21 @@
 const React = require('react');
 
-const App = function() {
-  return <div>Hello, world!</div>;
-};
+function App() {
+  return (
+    <div id='page'>
+      <header>
+        <h1>Main Header</h1>
+      </header>
+      <div id='content'>
+        <form>
+          <label>Enter a City and State
+            <input type='text' name='location' size='15' placeholder='Muncie, IN'/>
+          </label>
+          <input type='submit' name='get-weather' value='Get Weather'/>
+        </form>
+      </div>
+    </div>
+  );
+}
 
 module.exports = App;
